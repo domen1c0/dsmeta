7707a3f8f3984c14e27d31748f03a88640780b31
Paolo Domenico <paolodomenico777@gmail.com>
2022-09-10 13:38:12 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -11,16 +11,23 @@ function SalesCard() {
   const max = new Date()
   const min = new Date(new Date().setDate(new Date().getDate() - 365))
 
+  const [minDate, setMinDate] = useState(min)
+  const [maxDate, setMaxDate] = useState(max)
+
   const [sales, setSales] = useState<Sale[]>([])
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales`).then(response => {
-      setSales(response.data.content)
-    })
-  }, [])
+    const dmin = minDate.toISOString().slice(0, 10)
+    const dmax = maxDate.toISOString().slice(0, 10)
 
-  const [minDate, setMinDate] = useState(min)
-  const [maxDate, setMaxDate] = useState(max)
+    axios
+      .get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
+      .then(response => {
+        setSales(response.data.content)
+      })
+  }, [minDate,maxDate])
+
+  
 
   return (
     <div className="dsmeta-card">
@@ -63,7 +70,9 @@ function SalesCard() {
               return (
                 <tr key={sale.id}>
                   <td className="show992">{sale.id}</td>
-                  <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                  <td className="show576">
+                    {new Date(sale.date).toLocaleDateString()}
+                  </td>
                   <td>{sale.sellerName}</td>
                   <td className="show992">{sale.visited}</td>
                   <td className="show992">{sale.deals}</td>
